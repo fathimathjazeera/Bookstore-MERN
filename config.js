0a7b513a05a357111369b6b59d46a8907cd9e489
fathimathjazeera <jezijazeera@gmail.com>
2024-01-17 10:59:59 +0530

    added bookschema

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
 export const PORT =5555
+
+
+
+export const mongoDBURL ='mongodb+srv://jezi:jezidb@bookstore.xqrampn.mongodb.net/bookscollection?retryWrites=true&w=majority'
+
+
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { PORT, mongoDBURL} from './config.js'
+import mongoose from 'mongoose'
 const app = express()
 
 
-app.listen(PORT,()=>{
-    console.log(`App listening to port :${PORT}`)
+
+app.get('/',(req,res)=>{
+    console.log("re")
+    res.send("hello")
+})
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log("App connected to database")
+    app.listen(PORT,()=>{
+        console.log(`App listening to port :${PORT}`)
+    })
+}).catch((error)=>{
+    console.log(error)
 })
