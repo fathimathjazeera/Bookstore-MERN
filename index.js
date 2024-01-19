52f6305b8ab3a5a600ca5c4aff1ef54020656087
fathimathjazeera <jezijazeera@gmail.com>
2024-01-19 15:45:49 +0530

    updated book

--- index.js
+++ index.js
@@ -45,8 +45,39 @@ return res.status(200).json({
     }
 })
 
+app.get('/books/:id',async (req,res)=>{
+    try{
+      
+        const {id}= req.params
+const books = await Book.findById(id)
+return res.status(200).json({
+  
+    data:books
+})
+    }catch(error){
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
+})
 
-
+app.put('/books/:id',async(req,res)=>{
+    try{
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({
+                message:"Send all required fields:title, author, publishYear"
+            })
+        }
+        const {id} = req.params
+      const result =  await Book.findByIdAndUpdate(id,req.body)
+      if(!result){
+        return res.status(404).json({message:'Book not found'})
+      }
+      return res.status(200).send({message:'Book updated successfully'})
+    }catch(error){
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
+})
 
 mongoose.connect(mongoDBURL).then(()=>{
     console.log("App connected to database")
