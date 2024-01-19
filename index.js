1763f0d90dcb822154adcccd18a9fb56e5165e16
fathimathjazeera <jezijazeera@gmail.com>
2024-01-19 15:07:43 +0530

    created book

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 import express from 'express'
 import { PORT, mongoDBURL} from './config.js'
 import mongoose from 'mongoose'
+import { Book } from './models/bookModel.js'
 const app = express()
 
 
-
+app.use(express.json())
 app.get('/',(req,res)=>{
     console.log("re")
     res.send("hello")
 })
+
+app.post('/books',async(req,res)=>{
+   
+try {
+    if(!req.body.title || !req.body.author || !req.body.publishYear){
+        return res.status(400).send({
+            message:"Send all required fields:title, author, publishYear"
+        })
+    }
+    const newBook = {
+        title:req.body.title,
+        author: req.body.author,
+        publishYear:req.body.publishYear
+    }
+    const book = await Book.create(newBook)
+  return res.status(201).send(book)
+} catch (error) {
+    console.log(error)
+res.status(500).send({message:error.message})
+}
+})
+
+
+
+
 
 mongoose.connect(mongoDBURL).then(()=>{
     console.log("App connected to database")
@@ -16,5 +42,6 @@ mongoose.connect(mongoDBURL).then(()=>{
         console.log(`App listening to port :${PORT}`)
     })
 }).catch((error)=>{
-    console.log(error)
+    console.log(error,"err")
 })
+
